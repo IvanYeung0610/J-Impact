b87ac2afd6977afdea853bbfb3aa7c100d4fca92
Joshua Liu <jliu31@stuy.edu>
2023-06-06 14:03:17 -0400

    agsrhdf

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -2,6 +2,21 @@
 var socket = io();
 var messageform = document.getElementById("messageform");
 var nav_friends_link = document.getElementById("nav_friends_link")
+var all_group_buttons = document.getElementsByName("group_button")
+
+// We need a default group
+selected_group = all_group_buttons[0]
+all_group_buttons[0].style.backgroundColor = "red"
+socket.emit("select_group", all_group_buttons[0].id)
+
+for(let x = 0; x < all_group_buttons.length; x++){
+    all_group_buttons[x].addEventListener( 'click', (e) => {
+        socket.emit("select_group", all_group_buttons[x].id)
+        selected_group.style.backgroundColor = "#DEF2F1"
+        all_group_buttons[x].style.backgroundColor = "red"
+        selected_group = all_group_buttons[x]
+    })
+}
 
 socket.on('message', function (message) {
     console.log(message);
@@ -18,14 +33,15 @@ nav_friends_link.addEventListener('click', (e) => {
 messageform.addEventListener('submit', (e) => {
     // preventDefault stops the page from reloading
     e.preventDefault();
-    socket.send(document.getElementById("messageinput").value);
+    console.log("Emitted message");
+    socket.emit("message", document.getElementById("messageinput").value);
 })
 
 //notes for future redesign: we don't need to wait for the request to be completed before showing the message?
 var ajaxMessage = function (str) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        console.log(this.readyState);
+        // console.log(this.readyState);
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xhttp.responseText);
             document.getElementById("messages").innerHTML += response.user + ": " + response.value + "<br>";
@@ -34,7 +50,7 @@ var ajaxMessage = function (str) {
     xhttp.open("POST", "/homeajax");
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     var postVars = "messageText=" + str;
-    console.log(postVars);
+    // console.log(postVars);
     xhttp.send(postVars);
     var textField = document.getElementById("messageinput");
     textField.value = "";
