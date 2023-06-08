ac596b9251710de1beeea81e84b50d6ebbddbafe
Joshua Liu <jliu31@stuy.edu>
2023-06-07 23:30:37 -0400

    cloud

--- app/cloud.py
+++ app/cloud.py
@@ -9,6 +9,15 @@ with open("app/keys/secret.env", "r") as secret_key:
 cloudinary.config(
 cloud_name = "dg13dndup", 
 api_key = "536775968164576", 
-api_secret = secret_key,
+api_secret = key,
 secure = True
 )
+
+def upload_image(image_data):
+    result = cloudinary.uploader\
+    .upload(image_data, 
+    gravity = "center", width = 200, height = 200, crop = "scale", 
+    radius = 50, border="7px_solid_red", fetch_format="png")
+
+    return result.get("url")
+print(upload_image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuDMdvpVCd4NSqu0Bn1Sn6iN0nqi0QWjZxNg&usqp=CAU"))
